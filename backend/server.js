4ff0f51f2a74041cee96ac7a9e6f6948908e770c
Festim <krasniqif937@gmail.com>
2025-06-13 16:24:22 +0200

    commit

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,15 @@ app.use('/api/reviews',revieRoutes)
 
 
 
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
