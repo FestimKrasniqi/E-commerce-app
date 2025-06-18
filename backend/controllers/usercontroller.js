fcb47272809918dab07d8cff1db95c07dc811a99
Festim <krasniqif937@gmail.com>
2025-06-18 17:01:32 +0200

    Changed User Controller file

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -140,11 +140,12 @@ const forgetPassword = async (req, res) => {
 
       console.log("🔑 Reset Token (use this in Postman):", resetToken);
 
+      console.log(process.env.USER_EMAIL, process.env.USER_PASSWORD);
       const transporter = nodemailer.createTransport({
         service: "Gmail",
         auth: {
-          user: "krasniqif937@gmail.com",
-          pass: "rxmixlfunvjlybun",
+          user: process.env.USER_EMAIL || "",
+          pass: process.env.USER_PASSWORD || "",
         },
       });
 
--- backend/server.js
+++ backend/server.js
@@ -24,13 +24,6 @@ app.use('/api/reviews',revieRoutes)
 
 
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-//   });
-// }
 
 const PORT = process.env.PORT;
 
